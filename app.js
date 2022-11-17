836a0f77477dc94b95171e3f50d3b673c9a50b78
HowardWu5566 <ggg1237890@gmail.com>
2022-11-17 10:38:23 +0800

    add relations between user and todo models

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const bcrypt = require('bcryptjs')
+const db = require('./models')
+const Todo = db.Todo
+const User = db.User
 const app = express()
 const PORT = 3000
 
@@ -24,8 +27,11 @@ app.post('/users/login', (req, res) => {
 app.get('/users/register', (req, res) => {
   res.render('register')
 })
-app.post('/user/register', (req, res) => {
-  res.send('register')
+app.post('/users/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  User.create({ name, email, password })
+    .then(user => res.redirect('/'))
+
 })
 
 app.get('/users/logout', (req, res) => {
